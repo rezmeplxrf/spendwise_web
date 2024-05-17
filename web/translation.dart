7cb497408a423eff55dd75474d2c0430d4ce5171
Donghu <rezmeplx@rfway.org>
2024-05-17 09:58:35 +0900

    d

--- web/translation.dart
+++ web/translation.dart
@@ -2,31 +2,32 @@
 
 final Map<String, String> translationsKo = {
   'title': '현명한소비',
-  'subTitle': '온라인 쇼핑의 지출을 관리하고 지혜롭게 소비하세요.',
+  'subTitle': '온라인 쇼핑의 지출을 관리하고 지혜롭게 소비하세요',
   'features': '주요 기능',
   'tracking': '지출 관리',
   'trackingDetail':
       '여러 플랫폼의 주문 내역을 한 곳에서 확인하고 쉽게 검색하거나 엑셀, 스프레드시트와 호환되는 CSV파일로 저장하세요',
   'finder': '제품 검색',
-  'finderDetail': '여러 플랫폼의 제품을 쉽게 검색하세요',
+  'finderDetail': '살 것 목록을 작성하고 여러 플랫폼의 제품을 쉽게 검색하세요',
   'alerts': '핫딜 알림',
   'alertsDetail': '핫딜을 쉽게 확인하거나 검색하고 미리 알림을 받으세요',
   'contact': '질문이나 요청이 있으시다면',
   'contactButton': '문의하기',
   'cta': '현명한소비 다운로드',
   'videoTitle': '사용법 보기',
-  'brandlogos': '*각 상표는 해당 회사의 재산입니다',
+ 
 };
 
 final Map<String, String> translationEn = {
   'title': 'SpendWise',
-  'subTitle': 'Manage your expenses and shop in one place.',
+  'subTitle': 'Manage your expenses and shop in one place',
   'features': 'Features',
   'tracking': 'Expense Tracking',
   'trackingDetail':
       'View and search for purchase histories from multiple e-commerce platforms and export to CSV file securely.',
   'finder': 'Product Finder',
-  'finderDetail': 'Search for products from multiple platforms',
+  'finderDetail':
+      'Note things to buy later and search for products from multiple platforms',
   'alerts': 'Deal Alerts',
   'alertsDetail':
       'View and search for deals from multiple platforms and get notifications from your device.',
@@ -34,5 +35,5 @@ final Map<String, String> translationEn = {
   'contactButton': 'Contact Us',
   'cta': 'Ready to take control? Download SpendWise now!',
   'videoTitle': 'Check out demo',
-  'brandlogos': '*Each logo is the property of their respective owners.',
+ 
 };
